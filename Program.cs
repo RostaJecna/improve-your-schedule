b98169c3f5395740d71af0c2790da32ada977461
r0usta <levder@spsejecna.cz>
2023-12-06 11:26:40 +0100

    Upload on GitHub.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImproveYourSchedule
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ing. Mandik!");
+        }
+    }
+}
