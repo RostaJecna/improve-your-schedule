107d491085326c1ba46584895f7dbbad55d3b308
r0usta <levder@spsejecna.cz>
2023-12-14 13:42:29 +0100

    Create `Enum` classes for `Subject` class.

---
+++ Enums/SubjectAspect.cs
@@ -0,0 +1,12 @@
+namespace ImproveYourSchedule.Enums
+{
+    /// <summary>
+    /// Represents the type of a subject (theoretical, practical, both).
+    /// </summary>
+    public enum SubjectAspect
+    {
+        Theoretical,
+        Practical,
+        Both
+    }
+}
---
+++ Enums/SubjectType.cs
@@ -0,0 +1,68 @@
+    namespace ImproveYourSchedule.Enums
+    {
+        /// <summary>
+        /// Represents different types of subjects.
+        /// </summary>
+        public enum SubjectType
+        {
+            /// <summary>
+            /// Czech Language and Literature
+            /// </summary>
+            C = 0,
+
+            /// <summary>
+            /// English Language
+            /// </summary>
+            E = 1,
+
+            /// <summary>
+            /// Mathematics
+            /// </summary>
+            M = 2,
+
+            /// <summary>
+            /// Physical Education
+            /// </summary>
+            Phe = 3,
+
+            /// <summary>
+            /// Software Engineering
+            /// </summary>
+            Se = 4,
+
+            /// <summary>
+            /// Database Systems
+            /// </summary>
+            Ds = 5,
+
+            /// <summary>
+            /// Enterprise Information Systems
+            /// </summary>
+            Eis = 5,
+
+            /// <summary>
+            /// Computer Systems and Networks
+            /// </summary>
+            Csn = 6,
+
+            /// <summary>
+            /// Applied Mathematics
+            /// </summary>
+            Am = 7,
+
+            /// <summary>
+            /// IT Management Exercises
+            /// </summary>
+            Eit = 8,
+
+            /// <summary>
+            /// Web Applications
+            /// </summary>
+            Wa = 9,
+
+            /// <summary>
+            /// Technical Project
+            /// </summary>
+            Tp = 10
+        }
+    }
